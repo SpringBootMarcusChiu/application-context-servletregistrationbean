c3b4c1c1fc9cec2c1742a9659d2e454f1519c91c
Marcus Chiu <marcuschiu9@gmail.com>
2019-12-08 22:49:23 -0600

    README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+## Running Application
+- install java version 8
+- either:
+    - install maven
+        - <code>mvn clean compile</code>
+        - <code>mvn spring-boot:run</code>
+    - use maven wrapper
+        - <code>./mvnw clean compile</code>
+        - <code>./mvnw spring-boot:run</code>
+- open browser:
+  - http://localhost:8080/api/2/
+
+## Confluence Article
+- [http://confluence.marcuschiu.com/article](http://confluence.marcuschiu.com/display/NOT/Java+-+Spring+-+Application+Context+-+ServletRegistrationBean)
--- src/main/java/com/marcuschiu/example/servletregistrationbean/Application.java
+++ src/main/java/com/marcuschiu/example/servletregistrationbean/Application.java
@@ -2,32 +2,19 @@ package com.marcuschiu.example.servletregistrationbean;
 
 import com.marcuschiu.example.servletregistrationbean.config.CustomWebMvcConfigurer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	//    @Bean
-//    public ServletRegistrationBean apiV1() {
-//        DispatcherServlet dispatcherServlet = new DispatcherServlet();
-//
-//        XmlWebApplicationContext applicationContext = new XmlWebApplicationContext();
-//        applicationContext.setConfigLocation("classpath:/META-INF/spring/webmvc-context.xml");
-//        dispatcherServlet.setApplicationContext(applicationContext);
-//
-//        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(dispatcherServlet, "/api/1/*");
-//        servletRegistrationBean.setName("api-v1");
-//
-//        return servletRegistrationBean;
-//    }
 
 	@Bean
 	public ServletRegistrationBean apiV2() {
@@ -41,4 +28,18 @@ public class Application {
 		servletRegistrationBean.setName("api-v2");
 		return servletRegistrationBean;
 	}
+
+//    @Bean
+//    public ServletRegistrationBean apiV1() {
+//        DispatcherServlet dispatcherServlet = new DispatcherServlet();
+//
+//        XmlWebApplicationContext applicationContext = new XmlWebApplicationContext();
+//        applicationContext.setConfigLocation("classpath:/META-INF/spring/webmvc-context.xml");
+//        dispatcherServlet.setApplicationContext(applicationContext);
+//
+//        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(dispatcherServlet, "/api/1/*");
+//        servletRegistrationBean.setName("api-v1");
+//
+//        return servletRegistrationBean;
+//    }
 }
